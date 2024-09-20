fedde937686f3b03bc8d5e3836bff2da9eee0d5d
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-20 12:16:10 +0000

    add: shipping address model

--- store/models.py
+++ store/models.py
@@ -16,7 +16,7 @@ class Customer(models.Model):
 class Product(models.Model):
 	name = models.CharField(max_length=100)
 	price = models.FloatField()
-	# digital = models.BooleanField(default=False,null=True, blank=True)
+	digital = models.BooleanField(default=False,null=True, blank=True)
 
 	def __str__(self):
 		return self.name
@@ -37,3 +37,16 @@ class OrderItem(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+# shipping address model child to order model
+class ShippingAddress(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	address = models.CharField(max_length=200, null=False)
+	city = models.CharField(max_length=200, null=False)
+	county = models.CharField(max_length=200, null=False)
+	post_code = models.CharField(max_length=200, null=False)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.address
