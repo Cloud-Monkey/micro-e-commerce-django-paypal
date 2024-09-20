8b59297620d917fde74c8630cae8f27a2114cca5
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-20 11:50:35 +0000

    add: product model and order model

--- store/models.py
+++ store/models.py
@@ -11,3 +11,24 @@ class Customer(models.Model):
 
 	def __str__(self):
 		return self.name
+
+# product model
+class Product(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.FloatField()
+	# digital = models.BooleanField(default=False,null=True, blank=True)
+
+	def __str__(self):
+		return self.name
+
+# order model
+class Order(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+	date_ordered = models.DateTimeField(auto_now_add=True)
+	complete = models.BooleanField(default=False)
+	transaction_id = models.CharField(max_length=100, null=True)
+
+	def __str__(self):
+		return str(self.id)
+
+
