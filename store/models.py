9ab9124d5d3ed2990aec7b595abac3f12e051676
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-20 13:54:31 +0000

    edit: product model with category field

--- store/models.py
+++ store/models.py
@@ -14,6 +14,7 @@ class Customer(models.Model):
 
 # product model 
 class Product(models.Model):
+	category = models.CharField(max_length=100)
 	name = models.CharField(max_length=100)
 	price = models.FloatField()
 	color = models.CharField(max_length=20)
