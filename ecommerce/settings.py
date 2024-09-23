ec4cd0fe1b7e5171e315b2c8bddf8dee301b884a
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-23 15:48:45 +0000

    edit: debug to True settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-cloudmonkey-microecomme-m2tfe9h2r61.ws.codeinstitute-ide.net', 'button-mashers-11120a298309.herokuapp.com']
 
