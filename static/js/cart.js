5b72800409413e45c8df1fb19c916bff316706d8
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-24 09:52:27 +0000

    add: logic to check if user is logged in or not cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,10 +1,19 @@
 const updateBtns = document.getElementsByClassName('update-cart')
 
+// event listener for add to cart button
 for (i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function(){
+	updateBtns[i].addEventListener('click', function () {
 		var productId = this.dataset.product
 		var action = this.dataset.action
 		console.log('productId:', productId, 'Action:', action)
+// User is logged in or not
+		console.log('USER:', user)
+		if (user == 'AnonymousUser') {
+			console.log('User is not authenticated')
+
+		} else {
+			console.log('User is authenticated, sending data...')
+		}
 
 	})
 }
