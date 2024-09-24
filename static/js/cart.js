d6f3e934897f118c3f94e98cc57f6634174d85ee
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-24 10:26:10 +0000

    add: token generation in update user order function cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -3,10 +3,10 @@ const updateBtns = document.getElementsByClassName('update-cart')
 // event listener for add to cart button
 for (i = 0; i < updateBtns.length; i++) {
 	updateBtns[i].addEventListener('click', function () {
-		var productId = this.dataset.product
-		var action = this.dataset.action
+		const productId = this.dataset.product
+		const action = this.dataset.action
 		console.log('productId:', productId, 'Action:', action)
-// User is logged in or not
+		// User is logged in or not
 		console.log('USER:', user)
 		if (user == 'AnonymousUser') {
 			console.log('User is not authenticated')
@@ -16,4 +16,26 @@ for (i = 0; i < updateBtns.length; i++) {
 		}
 
 	})
+}
+
+// gets called when user is logged in POST request stringifys productID and action as a JSON object
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+		const url = '/update_item/'
+// create token workaround from django documentation
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
 }
--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import * 
+from django.http import JsonResponse
+
+from .models import *
 
 def store(request):
 	products = Product.objects.all()
@@ -33,3 +35,6 @@ def checkout(request):
 
 	context = {"items":items, "order":order}
 	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	return JsonResponse('Item was added', safe=False)
