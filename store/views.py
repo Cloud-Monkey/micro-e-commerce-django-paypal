99578581f8e6c5993a87a32488e7129e806cf922
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-25 10:34:33 +0000

    add: cart json.loads request cookies

--- store/views.py
+++ store/views.py
@@ -22,7 +22,7 @@ def store(request):
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-
+    
 	if request.user.is_authenticated:
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
@@ -30,9 +30,18 @@ def cart(request):
 		cartItems = order.get_cart_items
 	else:
 		#Create empty cart for now for non-logged in user
+		try:
+			cart = json.loads(request.COOKIES['cart'])
+		except:
+			cart = {}
+			print('CART:', cart)
+
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
+
+		for i in cart:
+			cartItems += cart[i]['quantity']
 
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
@@ -104,3 +113,4 @@ def processOrder(request):
 		print('User is not logged in')
 
 	return JsonResponse('Payment submitted..', safe=False)
+ 
