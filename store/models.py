498c1bdb646742fcd0e2f919e2c7a0e49416bc5b
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-27 13:46:37 +0000

    fix: add function to populate customer table on user creation models.py

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -11,6 +13,19 @@ class Customer(models.Model):
 
 	def __str__(self):
 		return self.name
+
+@receiver(post_save, sender=User)
+def create_customer(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(
+            user=instance,
+            name=instance.username,
+            email=instance.email
+        )
+
+# @receiver(post_save, sender=User)
+# def save_customer(sender, instance, **kwargs):
+#     instance.customer.save()
 
 # product model 
 class Product(models.Model):
