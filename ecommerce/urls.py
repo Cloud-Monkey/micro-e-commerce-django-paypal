3a041ef32b3a53e61e7b7b00015866c39980eb28
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-28 17:56:24 +0000

    edit: remove summernote from urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path("accounts/", include("allauth.urls")),
-    path('summernote/', include('django_summernote.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
