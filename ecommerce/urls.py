4b064e14e9c7b0fcd82b9fb45f64f9f73f1b64bd
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-28 10:13:01 +0000

    add: summernote urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path("accounts/", include("allauth.urls")),
+    path('summernote/', include('django_summernote.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
