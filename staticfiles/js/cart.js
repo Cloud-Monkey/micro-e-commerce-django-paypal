f03d1a8575dd50afb4c3ab653271d2446fd20b49
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-30 11:22:56 +0000

    add: collect static to update staticfiles directory

--- staticfiles/js/cart.js
+++ staticfiles/js/cart.js
@@ -1,32 +1,34 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+const updateBtns = document.getElementsByClassName('update-cart')
 
+// event listener for add to cart button
 for (i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
-		console.log('productId:', productId, 'Action:', action)
-		console.log('USER:', user)
+	updateBtns[i].addEventListener('click', function () {
+		const productId = this.dataset.product
+		const action = this.dataset.action
 
-		if (user == 'AnonymousUser'){
+		// User is logged in or not
+		if (user == 'AnonymousUser') {
 			addCookieItem(productId, action)
-		}else{
+		} else {
 			updateUserOrder(productId, action)
 		}
+
 	})
 }
 
-function updateUserOrder(productId, action){
+// gets called when user is logged in POST request stringifys productID and action as a JSON object
+function updateUserOrder(productId, action) {
 	console.log('User is authenticated, sending data...')
 
-		var url = '/update_item/'
-
+	const url = '/update_item/'
+	// create token workaround from django documentation
 		fetch(url, {
-			method:'POST',
-			headers:{
-				'Content-Type':'application/json',
-				'X-CSRFToken':csrftoken,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-CSRFToken': csrftoken,
 			}, 
-			body:JSON.stringify({'productId':productId, 'action':action})
+		body: JSON.stringify({ 'productId': productId, 'action': action })
 		})
 		.then((response) => {
 		   return response.json();
@@ -36,28 +38,33 @@ function updateUserOrder(productId, action){
 		});
 }
 
+// create cookie for cart when user is logged or not
 function addCookieItem(productId, action){
-	console.log('User is not authenticated')
+    console.log('User is not authenticated');
 
-	if (action == 'add'){
-		if (cart[productId] == undefined){
-		cart[productId] = {'quantity':1}
+    if (action == 'add'){
+        if (cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        } else {
+            cart[productId]['quantity'] += 1
+        }
+    }
 
-		}else{
-			cart[productId]['quantity'] += 1
-		}
-	}
+    if (action == 'remove'){
+        cart[productId]['quantity'] -= 1
 
-	if (action == 'remove'){
-		cart[productId]['quantity'] -= 1
+        if (cart[productId]['quantity'] <= 0){
+            console.log('Item should be deleted')
+            delete cart[productId];
+        }
+    }
 
-		if (cart[productId]['quantity'] <= 0){
-			console.log('Item should be deleted')
-			delete cart[productId];
-		}
-	}
-	console.log('CART:', cart)
-	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
-	
-	location.reload()
+    // Set the cookie with SameSite=Lax attribute
+    document.cookie = 'cart=' + JSON.stringify(cart) + 
+                      ';path=/;' +
+                      'max-age=604800;' + // 1 week
+                      'SameSite=Lax;' +
+                      (window.location.protocol === 'https:' ? 'Secure;' : '');
+    
+    location.reload()
 }
